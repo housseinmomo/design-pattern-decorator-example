fb73637ac10f611d71007de9e3ba104106ab922d
housseinmomo <smrobla34@gmail.com>
2022-12-23 14:17:11 +0100

    ajout de la property message

--- src/com/mugen/MessageNotification.java
+++ src/com/mugen/MessageNotification.java
@@ -6,10 +6,21 @@ public class MessageNotification implements Notification{
     public static String SMS = "SMS";
     public static String WHATAPPS = "WHATAPPS";
 
+    private String message;
+
     public MessageNotification(String messageNotification){
-        System.out.println("-> Message : " + messageNotification);
+        this.message = messageNotification;
+        System.out.println("-> Message : " + this.message);
     }
 
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
     @Override
     public String getTypeNotification() {
